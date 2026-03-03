7d1a5f70ecc5474cc6f11a5bb7f7abbeec97a821
yuangBI <yuangbi@outlook.com>
2026-03-02 21:52:19 -0600

    add mkscript and modify old script

--- .local/bin/chBackGround.sh
+++ .local/bin/chBackGround.sh
@@ -4,14 +4,21 @@ if [[ -a "$(dirname "$0")/clientid.sh" ]]; then
 else
 	echo "clientid file not find"
 fi
-if [[ -a ~/Pictures/new.jpg ]]; then
-	mv -f ~/Pictures/new.jpg ~/Pictures/wallpaper.jpg
+
+COUNT=30
+if [[ -a ~/Pictures/1.jpg ]]; then
+	mv -f ~/Pictures/1.jpg ~/Pictures/wallpaper.jpg
 fi
 pkill swaybg 2>/dev/null || true
 
 swaybg -i ~/Pictures/wallpaper.jpg -m fill & 
 
 Json=$(curl -H "Authorization: Client-ID $UNSPLASH_CLIENT_ID" \
-	https://api.unsplash.com/photos/random?orientation=landscape)
-	IMG_URL=$(echo "$Json" | jq -r '.urls.raw')
-	curl -L "$IMG_URL" -o "$HOME/Pictures/new.jpg"  
+	"https://api.unsplash.com/photos/random?orientation=landscape&count=$COUNT")
+
+
+mapfile -t IMG_URLS < <(echo "$Json" | jq -r 'if type=="array" then .[].urls.raw else .urls.raw end')
+
+for i in "${!IMG_URLS[@]}"; do
+	curl -L "${IMG_URLS[$i]}" -o "$HOME/Pictures/$((i + 1)).jpg"
+done
